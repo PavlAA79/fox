6093c663c4e573ceddca7c2a375bf174781093e1
PavlAA79 <55358397+PavlAA79@users.noreply.github.com>
2022-09-21 16:55:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "askname.h"
 int main(int argc, char **argv)
 {
- char first[255], last[255];
+ char first[255], last[255]; //imposter
  askname(first, last)
 printf("Hello, %s %s!\n", first, last);
  return 0;
